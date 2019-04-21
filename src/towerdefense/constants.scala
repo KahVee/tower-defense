8b498e6d4eba7b038d2ad8cec00a4b960ac4d87c
veeti <veeti@KahIdeapad>
2019-04-21 12:25:09 +0300

    Deleted Temp and some old image constants

--- src/towerdefense/constants.scala
+++ src/towerdefense/constants.scala
@@ -34,10 +34,5 @@ package object towerdefense {
 
   //TEMPORARY
   val DefaultImage = new Image(new FileInputStream("pics/grass.png"), TileSize, TileSize, true, false)
-  val PathImage = new Image(new FileInputStream("pics/path.png"), TileSize, TileSize, true, false)
-  val EntryImage = new Image(new FileInputStream("pics/entry.png"), TileSize, TileSize, true, false)
-  val ExitImage = new Image(new FileInputStream("pics/exit.png"), TileSize, TileSize, true, false)
-  val FarmImage = new Image(new FileInputStream("pics/farm.png"), TileSize, TileSize, true, false)
-  val TowerImage = new Image(new FileInputStream("pics/tower.png"), TileSize, TileSize, true, false)
   val EnemyImage = new Image(new FileInputStream("pics/enemy.png"), EnemySize, EnemySize, true, false)  
 }
